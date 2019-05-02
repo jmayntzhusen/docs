428947b7d76979a3d1b1f5a6daaec26fcaedd657
jmayntzhusen <36075913+jmayntzhusen@users.noreply.github.com>
2019-05-02 16:26:02 +0200

    Change build script to make git clone quietly as it gave errors in the pipeline

--- build-docs.ps1
+++ build-docs.ps1
@@ -1,7 +1,7 @@
 # If you want to get the API docs from the Umbraco CMS for local testing you can run this script, it is also this script that is used on the buildserver.
 
-# Comment out this command if you already have DocFX installed
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+# Comment out these commands if you already have Chocolatey and DocFX installed
+# Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install docfx -y
 
 git clone -q https://github.com/jmayntzhusen/Umbraco-CMS.git src
